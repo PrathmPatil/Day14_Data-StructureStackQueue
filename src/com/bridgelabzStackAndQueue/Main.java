ffc9b9aa181f4d05822d7eb28e4346870aa142d3
PrathmPatil <prathmpatil2818@gmail.com>
2023-01-11 17:05:46 +0530

    Using Linkedlist of queue

--- src/com/bridgelabzStackAndQueue/Main.java
+++ src/com/bridgelabzStackAndQueue/Main.java
@@ -1,6 +1,7 @@
 package com.bridgelabzStackAndQueue;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
@@ -8,33 +9,41 @@ public class Main {
 	public static void main(String []main)
 	{
 		
-		Stack<Integer> stack=new Stack<>();
+//		Stack<Integer> stack=new Stack<>();
+//		
+//		System.out.println("Stack are Empty: "+stack.empty());
+//		
+//		stack.push(70);
+//		stack.push(30);
+//		stack.push(56);
+//		System.out.println("Stack Peek Method");
+//		for(int i=stack.size();i>0;i--)
+//		{
+//			
+//			System.out.print(" -> "+stack.peek());
+//		 
+//		}
+//		System.out.println();
+//		System.out.println("Stack Pop Method");
+//		for(int i=stack.size();i>0;i--)
+//		{
+//			
+//			System.out.print(" -> "+stack.pop());
+//		 
+//		}
+		Queue<Integer> que=new LinkedList<Integer>();
+		que.add(56);
+		que.add(30);
+		que.add(70);
 		
-		System.out.println("Stack are Empty: "+stack.empty());
-		
-		stack.push(70);
-		stack.push(30);
-		stack.push(56);
-		System.out.println("Stack Peek Method");
-		for(int i=stack.size();i>0;i--)
-		{
-			
-			System.out.print(" -> "+stack.peek());
-		 
-		}
-		System.out.println();
-		System.out.println("Stack Pop Method");
-		for(int i=stack.size();i>0;i--)
-		{
-			
-			System.out.print(" -> "+stack.pop());
-		 
-		}
+	while(!que.isEmpty())
+	{
+		System.out.println(que.poll());
 		
 	}
 
 	
 	}
-
+}
 
 
